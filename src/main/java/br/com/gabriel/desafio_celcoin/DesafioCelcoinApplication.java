3e1281b596015cdce1c23fb361b7c56048ae8f3b
Gabriel Adriano Rodrigues <gabrieladriano@furb.br>
2024-06-03 11:36:19 -0300

    initial commit

---
+++ src/main/java/br/com/gabriel/desafio_celcoin/DesafioCelcoinApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.desafio_celcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCelcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCelcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/desafio_celcoin/DesafioCelcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.desafio_celcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCelcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
